8d6b4299d3eaf4aae940e538c2f33691d489458c
juraj@home <filanjuraj@gmail.com>
2020-07-03 13:14:33 +0200

    server - get injuries data

--- server/main.js
+++ server/main.js
@@ -15,6 +15,29 @@ app.get('/api/players', async (req, res) => {
                         });
   });
 
+app.get('/api/injuries', async (req, res) => {
+
+    fetch('https://www.premierinjuries.com/injury-table.php')
+        .then(function (response) {
+            switch (response.status) {
+                // status "OK"
+                case 200:
+                    return response.text();
+                // status "Not Found"
+                case 404:
+                    throw response;
+            }
+        })
+        .then(function (template) {
+            res.send(template);
+        })
+        .catch(function (response) {
+            // "Not Found"
+            res.send(response.statusText);
+        });
+
+  });
+
 // 404 - not found handling
 app.use((req, res, next) => {
   res.status(404).json({ error: '404: Not found' });
