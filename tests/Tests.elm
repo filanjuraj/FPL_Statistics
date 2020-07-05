695282e169630ca1c61fa8d3698c47f65c22d51a
juraj@home <filanjuraj@gmail.com>
2020-07-05 14:43:41 +0200

    testing - added json decoder

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,6 +2,7 @@ module Tests exposing (..)
 
 import Expect
 import Html.Parser
+import Json.Decode
 import Main exposing (..)
 import Test exposing (..)
 
@@ -10,7 +11,7 @@ import Test exposing (..)
 -- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
 
 
-testingPlayer =
+htmlTestingPlayer =
     """<tr class='player-row team_119'>
 \t\t\t\t\t\t\t<td><div class='mob-title'>Player</div>Callum Wilson</td>
 \t\t\t\t\t\t\t<td><div class='mob-title'>Reason</div>Suspended</td>
@@ -22,15 +23,83 @@ testingPlayer =
 \t\t\t\t\t\t</tr>"""
 
 
+jsonTestingPlayer =
+    """
+    {
+    "chance_of_playing_next_round": 100,
+    "chance_of_playing_this_round": 0,
+    "code": 75115,
+    "cost_change_event": 0,
+    "cost_change_event_fall": 0,
+    "cost_change_start": -6,
+    "cost_change_start_fall": 6,
+    "dreamteam_count": 1,
+    "element_type": 4,
+    "ep_next": "-0.2",
+    "ep_this": "0.0",
+    "event_points": 0,
+    "first_name": "Callum",
+    "form": "0.8",
+    "id": 67,
+    "in_dreamteam": false,
+    "news": "",
+    "news_added": "2020-06-24T19:00:18.390083Z",
+    "now_cost": 74,
+    "photo": "75115.jpg",
+    "points_per_game": "3.3",
+    "second_name": "Wilson",
+    "selected_by_percent": "4.2",
+    "special": false,
+    "squad_number": null,
+    "status": "a",
+    "team": 3,
+    "team_code": 91,
+    "total_points": 98,
+    "transfers_in": 903840,
+    "transfers_in_event": 97,
+    "transfers_out": 1783764,
+    "transfers_out_event": 895,
+    "value_form": "0.1",
+    "value_season": "13.2",
+    "web_name": "Callum Wilson",
+    "minutes": 2526,
+    "goals_scored": 8,
+    "assists": 3,
+    "clean_sheets": 3,
+    "goals_conceded": 49,
+    "own_goals": 0,
+    "penalties_saved": 0,
+    "penalties_missed": 0,
+    "yellow_cards": 9,
+    "red_cards": 0,
+    "saves": 0,
+    "bonus": 8,
+    "bps": 295,
+    "influence": "377.2",
+    "creativity": "347.8",
+    "threat": "701.0",
+    "ict_index": "141.8",
+    "influence_rank": 169,
+    "influence_rank_type": 21,
+    "creativity_rank": 91,
+    "creativity_rank_type": 11,
+    "threat_rank": 36,
+    "threat_rank_type": 21,
+    "ict_index_rank": 60,
+    "ict_index_rank_type": 20
+    }
+"""
+
+
 decodedPlayer =
     { firstName = "Callum"
     , secondName = "Wilson"
-    , team = 0
-    , webName = ""
-    , selectedBy = 0
-    , transfersIn = 0
-    , transfersOut = 0
-    , position = ""
+    , team = 3
+    , webName = "Callum Wilson"
+    , selectedBy = 4.2
+    , transfersIn = 97
+    , transfersOut = 895
+    , position = "FWD"
     }
 
 
@@ -50,21 +119,33 @@ player =
     }
 
 
-parseTestingPlayer : List Html.Parser.Node
-parseTestingPlayer =
-    Maybe.withDefault [] (Result.toMaybe (Html.Parser.run testingPlayer))
+parseHtmlTestingPlayer : List Html.Parser.Node
+parseHtmlTestingPlayer =
+    Maybe.withDefault [] (Result.toMaybe (Html.Parser.run htmlTestingPlayer))
 
 
 all : Test
 all =
     describe "A Test Suite"
-        [ test "Parsing of injuries from html" <|
+        [ test "Json player decoder" <|
             \_ ->
-                Expect.equal (List.length parseTestingPlayer) 1
+                let
+                    decodedPlayerFromJson =
+                        Json.Decode.decodeString Main.decodePlayer jsonTestingPlayer
+                in
+                case decodedPlayerFromJson of
+                    Ok result ->
+                        Expect.equal result decodedPlayer
+
+                    Err e ->
+                        Expect.fail (Json.Decode.errorToString e)
+        , test "Html parsing of injury" <|
+            \_ ->
+                Expect.equal (List.length parseHtmlTestingPlayer) 1
         , test "Is same name" <|
             \_ ->
                 Expect.equal
-                    (Main.isSameName (Maybe.withDefault (Html.Parser.Text "fail") (List.head parseTestingPlayer))
+                    (Main.isSameName (Maybe.withDefault (Html.Parser.Text "fail") (List.head parseHtmlTestingPlayer))
                         decodedPlayer
                     )
                     True
@@ -72,7 +153,7 @@ all =
             \_ ->
                 let
                     node =
-                        Maybe.withDefault (Html.Parser.Text "fail") (List.head parseTestingPlayer)
+                        Maybe.withDefault (Html.Parser.Text "fail") (List.head parseHtmlTestingPlayer)
                 in
                 case node of
                     Html.Parser.Element _ _ innerNodes ->
@@ -84,7 +165,7 @@ all =
             \_ ->
                 let
                     playerFromParsing =
-                        joinPlayersInjuries (decodedPlayer :: []) testingPlayer
+                        joinPlayersInjuries (decodedPlayer :: []) htmlTestingPlayer
                 in
                 case playerFromParsing of
                     Just list ->
